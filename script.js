4a9ca06c3c6396b42272c91cafd690614f5dfe71
Wasif Hasan <wasifhasancse@gmail.com>
2026-01-27 01:28:36 +0600

    As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Find all the even numbers from 78 to 98.
-let i = 78;
-while (i <= 98) {
-    if (i % 2 === 0) {
-        console.log('the even number is: ', i);  
-    }
-    i++;
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+for (let i = 1; i <= 10; i++) {
+  console.log("5 x", i, "=", 5 * i);
 }
