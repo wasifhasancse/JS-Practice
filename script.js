0af744dfac4a91073271b0a8b26ac7f41480bf0c
Wasif Hasan <wasifhasancse@gmail.com>
2026-01-27 01:18:46 +0600

    Find all the even numbers from 78 to 98.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-let i = 61;
-while (i <= 100) {
-    if (i % 2 !== 0) {
-        console.log('the odd number is: ', i);  
+// Find all the even numbers from 78 to 98.
+let i = 78;
+while (i <= 98) {
+    if (i % 2 === 0) {
+        console.log('the even number is: ', i);  
     }
     i++;
-    
 }
