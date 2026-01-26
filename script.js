d9e981f6c984ea1f86b127c141d914802d545656
Wasif Hasan <wasifhasancse@gmail.com>
2026-01-27 01:40:11 +0600

    Problem when the value of `i` does not increment & looping in infinite case

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 //     console.log(`5 x ${i} = ${5 * i}`);
 // }
 
-for (let i = 1; i <= 10; i++) {
-  console.log("5 x", i, "=", 5 * i);
+let i = 1;
+while (i <= 40) {
+    if (i % 2 !== 0) {
+        continue
+    }
+    console.log('even number is: ',i);
+    i++;
 }
