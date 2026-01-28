a9e6ab5d9eee260c681b178e46b9312c1a808620
Wasif Hasan <wasifhasancse@gmail.com>
2026-01-28 13:51:20 +0600

    add more problem

--- script.js
+++ script.js
@@ -99,6 +99,32 @@ console.log(reversedStatement);
 // .trim(): This removes the extra trailing space added during the final loop iteration.
 
 // task 5
-
+// Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
 let numArray = [1, 2, 3, 4, 5];
-let num2Array = numArray
+let copiedArray = [];
+for (let i = 0; i < numArray.length; i++) {
+  copiedArray[i] = numArray[i];
+}
+copiedArray[0] = 99;
+console.log("Original Array:", numArray);
+console.log("Copied Array:", copiedArray);
+
+// task 6
+// Given an array of student objects, print each student’s name and marks.
+let students = [
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 },
+];
+for (let i = 0; i < students.length; i++) {
+  console.log("Name:", students[i].name, ", Marks:", students[i].marks);
+}
+
+// task 7
+// Given a 2D array, update the value at second row first item to 99 and print the updated array.
+let matrix = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+matrix[1][0] = 99;
+console.log(matrix);
