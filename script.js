0871e7547ab05dc556a7ef53fddb0d4cc98458a1
Wasif Hasan <wasifhasancse@gmail.com>
2026-01-29 19:33:32 +0600

    object problem solving

--- script.js
+++ script.js
@@ -56,16 +56,122 @@ y = temp;
 console.log("x = " + x + ", y = " + y);
 // [x, y] = [y, x]; console.log('a = ' + x + ', b = ' + y);
 
-
 // Find the Last Digit of a Number Input: 12345 Output: 5
 a = x.toString();
 console.log(a[a.length - 1]);
 
-
 // Convert String to Number and Add Input: a = "10", b = 5 Output: 15
-a = '10';
+a = "10";
 b = 5;
-console.log(a + b); 
+console.log(a + b);
 c = parseInt(a) + b;
 console.log(c);
 
+a = [1, 2, 3, 4, 5];
+b = [];
+for (let num of a) {
+  if (num % 2 === 0) {
+    console.log(num + " is even");
+    b.push(num);
+  }
+}
+console.log(b);
+//use of includes() method
+a = ["banana", "apple", "orange", "mango"];
+b = a.includes("apple");
+console.log(b);
+
+// Object Manipulation
+const colors = {
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+  "golden rod": "#daa520",
+};
+// Adding a new property to the colors object
+colors["passenger capacity"] = 5;
+console.log(colors);
+// Adding a nested object as a property
+colors["car type"] = {
+  sedan: true,
+  suv: false,
+  coupe: true,
+};
+console.log(colors);
+
+// Find the lowest number in the array below. Input: [12, 45, 78, 34, 23, 89, 90] Output: 12
+a = [12, 45, 78, 34, 23, 89, 90];
+let min = a[0];
+for (let i = 0; i < a.length; i++) {
+  if (a[i] < min) {
+    min = a[i];
+  }
+}
+console.log(min);
+
+// Find the friend with the smallest name.
+const heights2 = ["rahim", "robin", "rafi", "ron", "rashed", "jon"];
+let minLength = heights2[0].length;
+let smallestNames = [];
+for (let i = 1; i < heights2.length; i++) {
+  if (heights2[i].length < minLength) {
+    minLength = heights2[i].length;
+    smallestNames = [heights2[i]];
+  } else if (heights2[i].length === minLength) {
+    smallestNames.push(heights2[i]);
+  }
+}
+console.log(smallestNames);
+
+// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+console.log(employees);
+let yearlyIncrement;
+let incrementSalary;
+let totalSalary = 0;
+for (let em of employees) {
+  yearlyIncrement = em["experience"] * em["increment"];
+  incrementSalary = em["starting"] + yearlyIncrement;
+  totalSalary += incrementSalary;
+  em["starting"] = incrementSalary;
+}
+console.log(employees);
+console.log("Total Salary Provided by the company in a month: ", totalSalary);
+
+// Accessing Nested Object Properties physics marks as output.
+const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  }
+};
+console.log(student["physics"]["marks"]);
+// Count the number of properties or Keys.
+console.log(Object.keys(student).length);
+console.log(Object.values(student));
+// Adding a new nasted object in a object
+student['physics']['chemistry'] = {
+  subject: "HSC Chemistry",
+  author: "Firoz Mahmud",
+  marks: 35,
+};
+console.log(student);
+
+// Iterate through Object Properties and log key, value and type of value.
+let enEmployee = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
+};
+for (let key in enEmployee) {
+  console.log('key:', key, '|', 'value:', enEmployee[key], '|', 'type:', typeof(enEmployee[key]))
+}
