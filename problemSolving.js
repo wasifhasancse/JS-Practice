48d0b1105bdccf66c54ff77a3f9d52d1ee6166f6
Wasif Hasan <wasifhasancse@gmail.com>
2026-02-05 22:29:30 +0600

    update

--- problemSolving.js
+++ problemSolving.js
@@ -42,7 +42,7 @@ function maps(x) {
   }
   return newArray;
 }
-let threeArray = [1, 2, 3, 4, 5];
+let threeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(
   "Original Array:",
   threeArray,
